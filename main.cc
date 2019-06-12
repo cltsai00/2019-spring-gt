0e733cc2d1b6dfbd0e77184dbf9f96993e3fd442
cltsai00 <43755969+cltsai00@users.noreply.github.com>
2019-06-12 12:33:56 +0800

    degree

--- main.cc
+++ main.cc
@@ -14,6 +14,39 @@ NetworkManager *nm = new NetworkManager();
 int main(int argc, char** argv){
 
     /* start your program */
+    nm->interpret("topo.txt");
+//    nm->print_all_v();
+//    nm->print_all_e();
+    Vertex* node = nm->get_all_nodes();
+    vector<string> node_v;
+    while(node!=NULL){
+    node_v.push_back(node->name);
+    node=node->next;
+    }
+//find the number of nodes with odd edges
+    int *degree;
+    degree = new int [node_v.size()];
+    for(int i=0;i<node_v.size();i++)
+      degree[i]=0;
+    int odd_node=0;
+    for(int i=0;i<node_v.size();i++){
+      for(int j=0;j<node_v.size();j++){
+        if(nm->connected(node_v[i],node_v[j])==0)
+          degree[i]++;   
+      }
+      if(degree[i]%2==1)
+        odd_node++;
+    }
+    for(int i=0;i<node_v.size();i++){
+      cout<<"degree of "<< node_v[i]<<" = "<<degree[i]<<endl;
+}
+   cout << endl <<node_v.size()<<endl;
 
+/*
+    Gplot *gp = new Gplot();
+    gp->gp_add(nm->elist);
+    gp->gp_dump(true);
+    gp->gp_export("plot");
+*/
     return 0;
 }
