d010fdd3840c7a645971a6e13f17960889fa3726
cltsai00 <43755969+cltsai00@users.noreply.github.com>
2019-06-14 01:22:34 +0800

    find result

--- main.cc
+++ main.cc
@@ -12,9 +12,11 @@ using namespace std;
 
 // create NetworkManager first
 NetworkManager *nm = new NetworkManager();
-vector<string> result_paths;
 
+vector<string> result;
 
+string find_result(int i,int tmp,string current, vector<string>nn,int *degree);
+int count_degree(int*degree);
 
 int main(int argc, char** argv){
     string current_node("a");
@@ -29,29 +31,18 @@ int main(int argc, char** argv){
       node=node->next;
     }
     vector<vector<Edge*>> avail_paths;
-    vector<vector<Edge*>> shortest_paths(node_v.size(),vector<Edge*>(node_v.size())); 
+    vector<vector<Edge*>> shortest_paths(1,vector<Edge*>(node_v.size())); 
     Path *path;
     path=new Path();
     path->append(nm->elist);
 
-//find the number of nodes with odd edges
+
     int *degree;
     degree = new int [node_v.size()];//degree of each node
     for(int i=0;i<node_v.size();i++)
       degree[i]=0;
+//find the number of nodes with odd edges
     int odd_node_count=0;//# of node with odd degrees
-
-
-/*    for(int i=0;i<node_v.size();i++){
-      for(int j=0;j<node_v.size();j++){
-        if(nm->connected(node_v[i],node_v[j])==0){
-          nm->connect_r(node_v[i],node_v[j]);
-          degree[i]++;
-       }   
-      }
-      if(degree[i]%2==1)
-        odd_node_count++;
-    }*/
 
     for(int i=0;i<node_v.size()-1;i++){
       for(int j=i+1;j<node_v.size();j++){
@@ -89,7 +80,6 @@ int main(int argc, char** argv){
 
 //avail_paths = path->find_paths(node_v[2],node_v[3]);
 //path->debug();
-//    path->debug();
 
     int s=0;
     for(int i=0;i<od;i=i+2){
@@ -118,7 +108,7 @@ cout<<shortest_paths.size()<<endl;
       for(int j=0;j<shortest_paths[i].size();j++){
      cout<<"2 ";
         for(int k=0;k<node_v.size();k++){ 
-          if(shortest_paths.at(i).at(j)->head->name==node_v[k]) cout<<"QQ"<<endl; 
+          if(shortest_paths.at(i).at(j)->head->name==node_v[k]||shortest_paths.at(i).at(j)->tail->name==node_v[k]) degree[k]++; 
    }
   }
  }
@@ -137,41 +127,55 @@ cout<<shortest_paths.size()<<endl;
 //find euler cycle with and without odd node
 
 
-
-/*    for(int i=0;i<dfs_paths.size();i++){
-      if(dfs_paths.at(i)->tail->name==dfs_paths.at(i+1)->head->name){
-      result_paths.at(result)=dfs_paths.at(i)->head->name;
-      result++;
-      result_paths.at(result)=dfs_paths.at(i)->tail->name;
-      result++;
-}
-}   */
-
-
 int tmp_node=0;//# of last node
 
-vector<string> result;
-      result.push_back(node_v[0]);
-/*    while(nm->elist!=NULL){  
-    for(int i=1;i<node_v.size();i++){
-      if(nm->connected(current_node,node_v[i])==0){
-        nm->disconnect(current_node,node_v[i]);
-        nm->disconnect(node_v[i],current_node);
-//        result.pushback(node_v[i]);
-        current_node = node_v[i];
-        
-        for(int j=0;j<node_v.size();j++){
-          if(nm->connected(current_node,node_v[j])==0&&node_v[j]!=result.back()){
-            nm->disconnect(current_node,node_v[i]);
-            nm->disconnect(node_v[i],current_node);
-            current_node = node_v[j];
-}           
-}       
-    }
-   }
-}*/
 
+    result.push_back(node_v[0]);
+
+int total_degree;
+//        if(nm->connected(current_node,node_v[1])==0&&node_v[1]!=result.back())
+//          find_result(1,tmp_node,current_node,node_v,degree);
+//        total_degree=count_degree(degree);
+
+     nm->print_all_e();
+/*
+   while(total_degree!=0){
+      for(int i=0;i<node_v.size();i++){
+        if(nm->connected(current_node,node_v[i])==0&&node_v[i]!=result.back()){
+          current_node = find_result(i,tmp_node,current_node,node_v,degree);
+          }
+        else if(nm->connected(current_node,node_v[i])==0&&degree[i]==1){
+          current_node = find_result(i,tmp_node,current_node,node_v,degree);
+}
+        total_degree=count_degree(degree);
+//        cout<<"total_degree is "<<total_degree<<endl<<"current_node is "<<current_node<<endl;
+//   for(int i=0;i<result.size();i++){
+//    cout<<result.at(i)<<"->";
+//}
+  }
+cout<<endl; 
+nm->print_all_e();
+}*/
+   //cout<<"total_degree is "<<total_degree<<endl;
 
     return 0;
 
+}
+
+
+string find_result(int i,int tmp,string current, vector<string>nn,int* degree){
+        nm->disconnect(current,nn[i]);
+        nm->disconnect(nn[i],current);
+        result.push_back(nn[i]);
+        current = nn[i];
+        degree[tmp]--;
+        degree[i]--;
+        cout<<"current node is"<<current;
+        return current; 
+      }
+int count_degree(int* degree){
+    int total=0;
+    for(int j=0;j<sizeof(degree);j++)
+      total+=degree[j];
+    return total;
 }
